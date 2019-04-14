7b178fba457b11c39f73a261fdc53fe1fa4088bb
Hardik <hthakkar8@gmail.com>
2019-04-14 14:56:08 +0530

    Updated Delete button access on restaurant Page

--- src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
@@ -4,7 +4,7 @@ import { Restaurant } from '../restaurant.model';
 import { RestaurantService } from '../restaurant.service';
 import { ReviewService } from '../review.service';
 import { DataService } from 'src/app/shared/data.service';
-
+import { AuthService } from '../../auth/auth.service';
 @Component({
   selector: 'app-restaurant-detail',
   templateUrl: './restaurant-detail.component.html',
@@ -15,7 +15,7 @@ export class RestaurantDetailComponent implements OnInit {
   restaurant: Restaurant;
   navLinks = [{path: 'general-details', label: 'Details'}, {path: 'reviews', label: 'Reviews'}];
   constructor(private activatedRoute: ActivatedRoute, private restaurantService: RestaurantService,
-              private dataService: DataService, private router: Router) { }
+              private dataService: DataService, private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.params['id'];
